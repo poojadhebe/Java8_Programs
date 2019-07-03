8a0078eb2b7bc24d92b6a8c9a7726488cd678f86
Pooja Dhebe <poojadhebe@gmail.com>
2019-07-03 21:05:26 +0530

    Collector code

--- StreamDemo/src/com/pooja/stream/CollectorExample.java
+++ StreamDemo/src/com/pooja/stream/CollectorExample.java
@@ -1,9 +1,42 @@
 package com.pooja.stream;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CollectorExample {
 	
 	public static void main(String args[]) {
-		
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(CollectorExample.class.getResourceAsStream("person.txt")))) {
+			Stream<String> stream1=br.lines();
+			List<Person> persons=new ArrayList<Person>();
+			stream1.forEach(line->{
+				String s[]=line.split(" ");
+				Person p=new Person(s[0],Integer.parseInt(s[1]));
+				persons.add(p);
+			});
+			System.out.println(persons);
+			
+			Optional<Person> minPerson=persons.stream().filter(p-> p.getAge()> 20).min(Comparator.comparing(Person::getAge));
+			System.out.println("Youngest person > 20::"+minPerson);
+			Map<Integer,Long> mapByAge=persons.stream().collect(Collectors.groupingBy(Person::getAge,Collectors.counting()));
+			System.out.println(mapByAge);
+			Map<Integer,Set<String>> mapByAgeWithName=persons.stream().collect(Collectors.groupingBy(Person::getAge,Collectors.mapping(Person::getName,Collectors.toSet() )));
+			System.out.println(mapByAgeWithName);
+			Map<Integer,Set<String>> mapByAgeWithTreeSet=persons.stream().collect(Collectors.groupingBy(Person::getAge,Collectors.mapping(Person::getName,Collectors.toCollection(TreeSet::new) )));
+			System.out.println(mapByAgeWithTreeSet);
+
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ StreamDemo/src/com/pooja/stream/Person.java
@@ -0,0 +1,50 @@
+package com.pooja.stream;
+
+public class Person {
+
+	private String name;
+	private int age;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Person [name=");
+		builder.append(name);
+		builder.append(", age=");
+		builder.append(age);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
